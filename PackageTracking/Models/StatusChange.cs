4a94eb841af2616914b3312b6c600bfd8ded316c
Benazzz <178609468+Benazzz@users.noreply.github.com>
2025-09-09 15:04:10 +0300

    Deleted unnecessary files

--- PackageTracking/Models/StatusChange.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PackageTracking.Models
-{
-    public class StatusChange
-    {
-        public PackageStatus Status { get; set; }
-        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
-    }
-}
